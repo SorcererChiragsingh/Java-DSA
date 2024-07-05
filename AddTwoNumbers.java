edd49cc6cd88a0afc949ee5a24db8f337c84235f
Chirag singh <chiragsingh8926@gmail.com>
2024-07-05 22:27:49 +0530

    LeetCode Q.2:- Add Two Numbers

---
+++ AddTwoNumbers.java
@@ -0,0 +1,49 @@
+// Leetcode:- https://leetcode.com/problems/add-two-numbers/description/
+// Leet Code Question no. 2
+// Add Two Numbers Problem DSA
+// Solution:- 
+
+
+// Recursion / LinkedList Problem
+
+// Easy
+
+// T.C = o(n)
+// S.C = o(n)
+
+/**
+Example 1:
+ Input: l1 = [2,4,3], l2 = [5,6,4]
+Output: [7,0,8]
+Explanation: 342 + 465 = 807.
+ */
+public class AddTwoNumbers 
+{
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) 
+    {
+        ListNode dummyHead = new ListNode(0);
+        ListNode tail = dummyHead;
+        int carry = 0;
+
+        while (l1 != null || l2 != null || carry != 0) 
+        {
+            int digit1 = (l1 != null) ? l1.val : 0;
+            int digit2 = (l2 != null) ? l2.val : 0;
+
+            int sum = digit1 + digit2 + carry;
+            int digit = sum % 10;
+            carry = sum / 10;
+
+            ListNode newNode = new ListNode(digit);
+            tail.next = newNode;
+            tail = tail.next;
+
+            l1 = (l1 != null) ? l1.next : null;
+            l2 = (l2 != null) ? l2.next : null;
+        }
+
+        ListNode result = dummyHead.next;
+        dummyHead.next = null;
+        return result;
+    }
+}
