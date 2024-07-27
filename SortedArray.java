277b2a45075c0983344f2e3fd16675a71552cbe2
Chirag singh <chiragsingh8926@gmail.com>
2024-07-27 23:07:48 +0530

    LeetCode Q.26:-Remove Duplicate From Sorted Array

--- SortedArray.java
+++ SortedArray.java
@@ -3,8 +3,10 @@
 // Remove Duplicates from Sorted Array Problem DSA
 // Solution:- https://www.youtube.com/watch?v=JHzdGAh_6mk&ab_channel=CollegeWallah
 
-public class SortedArray {
-    public static void main(String[] args) {
+public class SortedArray 
+{
+    public static void main(String[] args) 
+    {
         // Initialize an array with duplicates
         int[] nums = {1, 1, 2, 2, 3, 3, 4, 5, 5};
 
@@ -17,17 +19,21 @@ public class SortedArray {
         // Print the result
         System.out.println("The new length after removing duplicates is: " + newLength);
         System.out.print("The array after removing duplicates is: ");
-        for (int i = 0; i < newLength; i++) {
+        for (int i = 0; i < newLength; i++) 
+        {
             System.out.print(nums[i] + " ");
         }
     }
 
-    public int removeDuplicates(int[] nums) {
+    public int removeDuplicates(int[] nums) 
+    {
         if (nums.length == 0) return 0;
 
         int j = 0;
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[j] != nums[i]) {
+        for (int i = 1; i < nums.length; i++) 
+        {
+            if (nums[j] != nums[i]) 
+            {
                 nums[++j] = nums[i];
             }
         }
