93bd90cdbc2054caf3b34002737988d197e87b40
Chirag singh <chiragsingh8926@gmail.com>
2024-09-10 20:52:42 +0530

    LeetCode Q.2807:- Insert Greatest Common Divisor

--- InsertGreatestCommonDivisorLinkedList.java
+++ InsertGreatestCommonDivisorLinkedList.java
@@ -8,8 +8,8 @@
 
 // Medium
 
-// T.C = o()
-// S.C = o()
+// T.C = o(n)
+// S.C = o(1)
 
 // Given the head of a linked list head, in which each node contains an integer value.
 // Between every pair of adjacent nodes, insert a new node with a value equal to the greatest common divisor of them.
