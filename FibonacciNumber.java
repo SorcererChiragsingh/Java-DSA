7fcebb8c3c94c78a2fe5908f149403a71a76b1ae
Chirag singh <chiragsingh8926@gmail.com>
2024-09-24 22:41:16 +0530

    LeetCode Q.509:-Fibonacci Number

--- FibonacciNumber.java
+++ FibonacciNumber.java
@@ -4,12 +4,25 @@
 // Solution:- 
 
 
-// Recursion Problem
+// Recursion; Math; Dynamic Programming; Memoization Problem
 
 // Easy
 
 // T.C = o(n)
 // S.C = o(n)
+
+// The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence, such that each number is the sum of the two preceding ones, starting from 0 and 1. That is,
+// F(0) = 0, F(1) = 1
+// F(n) = F(n - 1) + F(n - 2), for n > 1.
+
+
+/**
+Example 1:
+
+Input: n = 2
+Output: 1
+Explanation: F(2) = F(1) + F(0) = 1 + 0 = 1.
+ */
 
 public class FibonacciNumber 
 {
@@ -32,5 +45,13 @@ public class FibonacciNumber
         } 
          
       return c; 
+    }
+    public static void main(String[] args) 
+    {
+        FibonacciNumber fibonacci = new FibonacciNumber();
+        int n = 10; // Example value for Fibonacci sequence
+
+        System.out.println("Fibonacci of " + n + " (recursive): " + fibonacci.fib(n));
+        System.out.println("Fibonacci of " + n + " (iterative): " + fibonacci.fibnoci(n));
     }
 }
