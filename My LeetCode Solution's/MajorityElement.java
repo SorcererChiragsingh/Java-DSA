31767e25cd8cdf349e34a5fd6527152903777048
Chirag singh <chiragsingh8926@gmail.com>
2024-10-17 20:37:36 +0530

    LeetCode Q.169:-Majority Element

--- My LeetCode Solution's/MajorityElement.java
+++ My LeetCode Solution's/MajorityElement.java
@@ -6,6 +6,17 @@
 // Easy
 // Moore's voting algorithm
 
+/**
+Given an array nums of size n, return the majority element.
+The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array. 
+*/
+
+/**
+Example 1:
+
+Input: nums = [3,2,3]
+Output: 3
+*/
 public class MajorityElement 
 {
     public static void main(String[] args)
