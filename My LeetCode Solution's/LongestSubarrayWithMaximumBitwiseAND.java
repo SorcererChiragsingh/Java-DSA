7fff1f2d37100cd485b11c575d829df89d781ef7
Chirag singh <chiragsingh8926@gmail.com>
2024-10-17 20:35:27 +0530

    LeetCode Q.2419:-Longest Subarray With Maximum Bitwise AND

--- My LeetCode Solution's/LongestSubarrayWithMaximumBitwiseAND.java
+++ My LeetCode Solution's/LongestSubarrayWithMaximumBitwiseAND.java
@@ -3,7 +3,6 @@
 // Longest Subarray With Maximum Bitwise AND Problem DSA
 // Solution:- https://leetcode.com/problems/longest-subarray-with-maximum-bitwise-and/solutions/5784657/simple-for-beginners-with-explanation
 
-// 
 // Array; Bit Manipulation; Brainteaser
 
 // Medium
@@ -11,12 +10,14 @@
 // T.C = o(n)
 // S.C = o(1)
 
-// You are given an integer array nums of size n.
-// Consider a non-empty subarray from nums that has the maximum possible bitwise AND.
-// In other words, let k be the maximum value of the bitwise AND of any subarray of nums. Then, only subarrays with a bitwise AND equal to k should be considered.
-// Return the length of the longest such subarray.
-// The bitwise AND of an array is the bitwise AND of all the numbers in it.
-// A subarray is a contiguous sequence of elements within an array.
+/**
+You are given an integer array nums of size n.
+Consider a non-empty subarray from nums that has the maximum possible bitwise AND.
+In other words, let k be the maximum value of the bitwise AND of any subarray of nums. Then, only subarrays with a bitwise AND equal to k should be considered.
+Return the length of the longest such subarray.
+The bitwise AND of an array is the bitwise AND of all the numbers in it.
+A subarray is a contiguous sequence of elements within an array.
+*/
 
 /**
  Example 1:
