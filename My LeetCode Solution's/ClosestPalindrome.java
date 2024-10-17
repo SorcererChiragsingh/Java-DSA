d91ce062d1519691acbe9aa297488c72a5afda68
Chirag singh <chiragsingh8926@gmail.com>
2024-10-17 20:03:02 +0530

    LeetCode Q.564:-Find The Closest Palindrome

--- My LeetCode Solution's/ClosestPalindrome.java
+++ My LeetCode Solution's/ClosestPalindrome.java
@@ -13,10 +13,11 @@
 // T.C = o(n)
 // S.C = o(n)
 
-// Given a string n representing an integer, return the closest integer (not including itself), 
-// which is a palindrome. If there is a tie, return the smaller one.
-
-// The closest is defined as the absolute difference minimized between two integers.
+/**
+Given a string n representing an integer, return the closest integer (not including itself), 
+which is a palindrome. If there is a tie, return the smaller one.
+The closest is defined as the absolute difference minimized between two integers.
+*/
 
 import java.util.HashSet;
 import java.util.Set;
