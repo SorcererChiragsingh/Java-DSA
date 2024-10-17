2b8aab50dba875586ae55ea3c34c14b6070a17a6
Chirag singh <chiragsingh8926@gmail.com>
2024-10-17 20:06:43 +0530

    LeetCode Q.509:-Fibonacci Number

--- My LeetCode Solution's/FibonacciNumber.java
+++ My LeetCode Solution's/FibonacciNumber.java
@@ -11,10 +11,11 @@
 // T.C = o(n)
 // S.C = o(n)
 
-// The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence, such that each number is the sum of the two preceding ones, starting from 0 and 1. That is,
-// F(0) = 0, F(1) = 1
-// F(n) = F(n - 1) + F(n - 2), for n > 1.
-
+/**
+The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence, such that each number is the sum of the two preceding ones, starting from 0 and 1. That is,
+F(0) = 0, F(1) = 1
+F(n) = F(n - 1) + F(n - 2), for n > 1.
+*/
 
 /**
 Example 1:
