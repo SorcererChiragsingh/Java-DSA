7de4ef9722a304b9cf99ff3d36c5658e555cb81b
Chirag singh <chiragsingh8926@gmail.com>
2024-10-17 19:51:25 +0530

    LeetCode Q.1009:-Complement Of Base 10 Integer

--- My LeetCode Solution's/BitWiseComplement.java
+++ My LeetCode Solution's/BitWiseComplement.java
@@ -4,7 +4,7 @@
 // Solution:- https://www.youtube.com/watch?v=0fwrMYPcGQ0&ab_channel=CodeHelp-byBabbar ; https://www.youtube.com/watch?v=QvmnMDzzGxk&ab_channel=EngineeringDigest
 
 // https://leetcode.com/problems/complement-of-base-10-integer/solutions/4456862/using-bit-masking-beats-100-java
-// 
+
 // Basic Maths Problem
 
 // Easy
@@ -12,9 +12,19 @@
 // T.C = o(n)
 // S.C = o(n)
 
-// The complement of an integer is the integer you get when you flip all the 0's to 1's and all the 1's to 0's in its binary representation.
-// For example, The integer 5 is "101" in binary and its complement is "010" which is the integer 2.
+/** 
+ The complement of an integer is the integer you get when you flip all the 0's to 1's and all the 1's to 0's in its binary representation.
+ For example, The integer 5 is "101" in binary and its complement is "010" which is the integer 2.
+ Given an integer n, return its complement.
+*/
 
+/**
+Example 1:
+
+Input: n = 5
+Output: 2
+Explanation: 5 is "101" in binary, with complement "010" in binary, which is 2 in base-10.
+ */
 public class BitWiseComplement 
 {
     public static void main(String[] args)
