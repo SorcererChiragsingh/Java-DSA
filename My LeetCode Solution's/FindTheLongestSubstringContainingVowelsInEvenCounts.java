3db5490b3ce7427bd50dc63d8e3ee653c26cf65c
Chirag singh <chiragsingh8926@gmail.com>
2024-10-17 20:08:32 +0530

    LeetCode Q.1371:-Find The Longest Substring Containing Vowels In Even Counts

--- My LeetCode Solution's/FindTheLongestSubstringContainingVowelsInEvenCounts.java
+++ My LeetCode Solution's/FindTheLongestSubstringContainingVowelsInEvenCounts.java
@@ -10,10 +10,10 @@
 // T.C = o(n)
 // S.C = o(1)
 
-// Given the string s, return the size of the longest substring containing each vowel an even number of times. 
-// That is, 'a', 'e', 'i', 'o', and 'u' must appear an even number of times.
-
-
+/* 
+Given the string s, return the size of the longest substring containing each vowel an even number of times. 
+That is, 'a', 'e', 'i', 'o', and 'u' must appear an even number of times.
+*/
 
 /** 
 Example 1:
