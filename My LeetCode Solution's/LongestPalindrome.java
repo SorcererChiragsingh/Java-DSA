9bab19cb8a7fb3bdf0b00b8bc258823b4859a574
Chirag singh <chiragsingh8926@gmail.com>
2024-10-17 20:33:09 +0530

    LeetCode Q.409:-Longest Palindrome

--- My LeetCode Solution's/LongestPalindrome.java
+++ My LeetCode Solution's/LongestPalindrome.java
@@ -3,13 +3,26 @@
 // Longest Palindrome Problem DSA
 // Solution:- 
 
-// 
 // Hash Map & Greedy Problem
 
 // Easy
 
 // T.C = o(n)
 // S.C = o(n)
+
+/**
+Given a string s which consists of lowercase or uppercase letters, return the length of the longest 
+palindrome that can be built with those letters.
+Letters are case sensitive, for example, "Aa" is not considered a palindrome.
+*/
+
+/**
+Example 1:
+
+Input: s = "abccccdd"
+Output: 7
+Explanation: One longest palindrome that can be built is "dccaccd", whose length is 7.
+*/
 
 import java.util.HashMap;
 import java.util.Map;
