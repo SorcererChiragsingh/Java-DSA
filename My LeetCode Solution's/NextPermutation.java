e9d244ab40ecaf1e4f1830f65904f2652a7e8554
Chirag singh <chiragsingh8926@gmail.com>
2024-10-18 21:27:34 +0530

    LeetCode Q.31:-Next Permutation

--- My LeetCode Solution's/NextPermutation.java
+++ My LeetCode Solution's/NextPermutation.java
@@ -3,8 +3,32 @@
 // Next Permutation Problem DSA
 // Solution:- https://www.youtube.com/watch?v=IhsUbEMfIbY&ab_channel=AlgorithmsMadeEasy; https://www.youtube.com/watch?v=JDOXKqF60RQ&ab_channel=takeUforward
 
+// Array; Two Pointers Problems
 // Medium
-// 
+// T.C= o()
+// S.C= o()
+
+/**
+A permutation of an array of integers is an arrangement of its members into a sequence or linear order.
+For example, for arr = [1,2,3], the following are all the permutations of arr: [1,2,3], [1,3,2], [2, 1, 3], [2, 3, 1], [3,1,2], [3,2,1].
+
+The next permutation of an array of integers is the next lexicographically greater permutation of its integer. 
+More formally, if all the permutations of the array are sorted in one container according to their lexicographical order, then the next permutation of that array is the permutation that follows it in the sorted container. 
+If such arrangement is not possible, the array must be rearranged as the lowest possible order (i.e., sorted in ascending order).
+For example, the next permutation of arr = [1,2,3] is [1,3,2].
+Similarly, the next permutation of arr = [2,3,1] is [3,1,2].
+While the next permutation of arr = [3,2,1] is [1,2,3] because [3,2,1] does not have a lexicographical larger rearrangement.
+Given an array of integers nums, find the next permutation of nums.
+
+The replacement must be in place and use only constant extra memory.
+*/
+
+ /**
+Example 1:
+
+Input: nums = [1,2,3]
+Output: [1,3,2]
+*/
 import java.util.Arrays;
 
 public class NextPermutation 
