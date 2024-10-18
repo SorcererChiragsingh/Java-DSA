9eecf6d9ce9145375cbc791679e6a38e0e4780a9
Chirag singh <chiragsingh8926@gmail.com>
2024-10-18 22:53:05 +0530

    LeetCode Q.189:-Rotate Array

--- My LeetCode Solution's/RotateArray.java
+++ My LeetCode Solution's/RotateArray.java
@@ -3,9 +3,43 @@
 // Rotate Array Problem DSA
 // Solution:- https://www.youtube.com/watch?v=a8rSdfcHouw&ab_channel=CollegeWallah
 
-//DIFFICULTY:- Medium
+// Array; Math; Two Pointers Problem
+// Medium
+
+// T.C = o()
+// S.C = o()
+
+/**
+Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
+*/
+
+/**
+Example 1:
+
+Input: nums = [1,2,3,4,5,6,7], k = 3
+Output: [5,6,7,1,2,3,4]
+Explanation:
+rotate 1 steps to the right: [7,1,2,3,4,5,6]
+rotate 2 steps to the right: [6,7,1,2,3,4,5]
+rotate 3 steps to the right: [5,6,7,1,2,3,4]
+*/
 public class RotateArray  
 {
+    public static void main(String[] args) 
+    {
+        RotateArray ra = new RotateArray();
+        
+        int[] nums = {1, 2, 3, 4, 5, 6, 7};  // Example array
+        int k = 3;  // Number of rotations
+        
+        ra.rotate(nums, k);
+        
+        System.out.println("Array after rotation: ");
+        for (int num : nums) 
+        {
+            System.out.print(num + " ");
+        }
+    }
     // Reverse Function
     public void reverse(int []nums, int start, int end)
     {
