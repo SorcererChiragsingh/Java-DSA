dd5472af8e0437d13f2f3bfef3f8791a285ac62e
Chirag singh <chiragsingh8926@gmail.com>
2024-10-18 20:01:44 +0530

    LeetCode Q.53:-Maximum Subarray

--- My LeetCode Solution's/MaximumSubArray.java
+++ My LeetCode Solution's/MaximumSubArray.java
@@ -3,9 +3,27 @@
 // Maximum Subarray Problem DSA
 // Solution:- https://www.youtube.com/watch?v=usxn9PV6Fdo&ab_channel=Technosage
 
+// Array; Divide and Conquer; Dynamic Programming
 // Medium
 // Kadane's algorithm
 
+/**
+Given an integer array nums, find the subarray with the largest sum, and return its sum.
+*/
+
+/**
+Example 1:
+
+Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+Output: 6
+Explanation: The subarray [4,-1,2,1] has the largest sum 6.
+
+Example 2:
+
+Input: nums = [1]
+Output: 1
+Explanation: The subarray [1] has the largest sum 1.
+*/
 public class MaximumSubArray 
 {
     public static void main(String[] args) 
