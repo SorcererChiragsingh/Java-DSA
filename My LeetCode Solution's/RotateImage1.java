43516bb15b9ba97c862e7b1e6c060daf1aae4c83
Chirag singh <chiragsingh8926@gmail.com>
2024-10-18 22:58:44 +0530

    LeetCode Q.48:-Rotate Image1

--- My LeetCode Solution's/RotateImage1.java
+++ My LeetCode Solution's/RotateImage1.java
@@ -3,10 +3,20 @@
 // Rotate Image Problem DSA
 // Solution:- https://www.youtube.com/watch?v=Z0R2u6gd3GU&ab_channel=takeUforward
 
+// Array; Math; Matrix Problems
 // Medium
-// 
+// T.C = o()
+// S.C = o()
 
+/**
+You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).
+You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation.
+*/
 
+/**
+Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+Output: [[7,4,1],[8,5,2],[9,6,3]]
+*/
 public class RotateImage1 
 {
     public static void rotate(int[][] matrix) 
