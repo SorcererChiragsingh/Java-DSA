2ddb42f9b390c424980925a75dee291ea4f1ac5b
Chirag singh <chiragsingh8926@gmail.com>
2024-10-18 23:25:26 +0530

    LeetCode Q.75:-Sort colors

--- My LeetCode Solution's/SortColors.java
+++ My LeetCode Solution's/SortColors.java
@@ -3,21 +3,25 @@
 // Sort Colors Problem DSA
 // Solution:- https://www.youtube.com/watch?v=HuS-ZsnEdbc&ab_channel=Codebix
 
-// Array /Two Pointers /Sorting Problem
-
+// Array; Two Pointers; Sorting Problem
 // Medium
+
 // T.C = o()
 // S.C = o()
+
+/**
+Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
+We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
+You must solve this problem without using the library's sort function.
+*/
 
 /** 
 Example 1:
 Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
-
 We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
 
 Input: nums = [2,0,2,1,1,0]
 Output: [0,0,1,1,2,2]
-
 */
 
 public class SortColors 
