47a121ffdae77be3ceb8dbd2fc3eda4de6704862
Chirag singh <chiragsingh8926@gmail.com>
2024-10-18 21:19:04 +0530

    LeetCode Q.283:-Move Zeroes To End

--- My LeetCode Solution's/MoveZeroes.java
+++ My LeetCode Solution's/MoveZeroes.java
@@ -3,9 +3,38 @@
 // Move Zeros to end Problem DSA
 // Solution:- https://www.youtube.com/watch?v=AgiGd763srg&ab_channel=Technosage
 
+// Array; Two Pointers Problems
+// Easy
 
+// T.C = o()
+// S.C = o()
+
+/**
+Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+Note that you must do this in-place without making a copy of the array.
+*/
+
+/**
+Example 1:
+
+Input: nums = [0,1,0,3,12]
+Output: [1,3,12,0,0]
+*/
 class MoveZeroes 
 {
+    public static void main(String[] args) 
+    {
+        MoveZeroes mz = new MoveZeroes();
+        
+        int[] nums = {0, 1, 0, 3, 12};  // Example array
+        mz.moveZeroes(nums);
+        
+        System.out.println("Array after moving zeroes: ");
+        for (int num : nums) 
+        {
+            System.out.print(num + " ");
+        }
+    }
     public void moveZeroes(int[] nums) 
     {
         int count =0;
