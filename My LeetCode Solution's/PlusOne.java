9ee2833dd76a8723971d384a01e72e4307284303
Chirag singh <chiragsingh8926@gmail.com>
2024-10-18 21:50:26 +0530

    LeetCode Q.66:-Plus One

--- My LeetCode Solution's/PlusOne.java
+++ My LeetCode Solution's/PlusOne.java
@@ -3,13 +3,28 @@
 // Plus One Problem DSA
 // Solution:- https://www.youtube.com/watch?v=8fbzKTSCvzo&ab_channel=Codebix ; https://www.youtube.com/watch?v=iBk_JyxxuN8&t=312s&ab_channel=Technosage
 
-// 
-// Basic Maths Problem
-
+// Array; Maths Problem
 // Easy
 
 // T.C = o(n)
 // S.C = o(n)
+
+/**
+You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer. 
+The digits are ordered from most significant to least significant in left-to-right order. 
+The large integer does not contain any leading 0's.
+Increment the large integer by one and return the resulting array of digits.
+*/
+
+/**
+Example 1:
+
+Input: digits = [1,2,3]
+Output: [1,2,4]
+Explanation: The array represents the integer 123.
+Incrementing by one gives 123 + 1 = 124.
+Thus, the result should be [1,2,4].
+*/
 import java.util.Arrays;
 
 public class PlusOne 
