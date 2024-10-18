1b3bfd7815e7642aee16d9dda2a71567b0d2d403
Chirag singh <chiragsingh8926@gmail.com>
2024-10-18 21:53:20 +0530

    LeetCode Q.231:-Power Of Two Integer

--- My LeetCode Solution's/PowerOfTwo.java
+++ My LeetCode Solution's/PowerOfTwo.java
@@ -4,15 +4,17 @@
 // Solution:- https://www.youtube.com/watch?v=0fwrMYPcGQ0&ab_channel=CodeHelp-byBabbar ; https://www.youtube.com/watch?v=ta17SZ_t-_M&ab_channel=Technosage
 
 // https://leetcode.com/problems/power-of-two/solutions/4748857/8-approaches-c-java-python-javascript-explained
-// Basic Maths Problem
 
+// Math; Bit Manipulation; Recursion Problem
 // Easy
 
 // T.C = o(n)
 // S.C = o(n)
+/**
+Given an integer n, return true if it is a power of two. Otherwise, return false.
+An integer n is a power of two, if there exists an integer x such that n == 2x.
+*/
 
-// Given an integer n, return true if it is a power of two. Otherwise, return false.
-// An integer n is a power of two, if there exists an integer x such that n == 2x.
 /**
  Example 1:
 
