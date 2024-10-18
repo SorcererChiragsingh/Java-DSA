0d70716fa88d50b87b03c98693ffda2526c1e0f3
Chirag singh <chiragsingh8926@gmail.com>
2024-10-18 22:20:37 +0530

    LeetCode Q.7:-Reverse Integer

--- My LeetCode Solution's/ReverseInteger.java
+++ My LeetCode Solution's/ReverseInteger.java
@@ -3,15 +3,24 @@
 // Reverse Integer Problem DSA
 // Solution:- https://www.youtube.com/watch?v=V5kYHHRoMlk&ab_channel=Technosage ; https://www.youtube.com/watch?v=0fwrMYPcGQ0&ab_channel=CodeHelp-byBabbar
 
-// 
 // Basic Maths Problem
-
 // Medium
 
 // T.C = o(n)
 // S.C = o(n)
 
+/**
+Given a signed 32-bit integer x, return x with its digits reversed. 
+If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+Assume the environment does not allow you to store 64-bit integers (signed or unsigned). 
+*/
 
+/**
+Example 1:
+
+Input: x = 123
+Output: 321 
+*/
 public class ReverseInteger 
 {
     public static void main(String[] args)
