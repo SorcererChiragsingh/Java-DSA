bc49e5e090a62bb2950f11cc83d1bafb6a04bd62
Chirag singh <chiragsingh8926@gmail.com>
2024-10-18 23:21:02 +0530

    LeetCode Q.136:-Single Number

--- My LeetCode Solution's/SingleNumber.java
+++ My LeetCode Solution's/SingleNumber.java
@@ -3,17 +3,20 @@
 // Single Number Problem DSA
 // Solution:- https://www.youtube.com/watch?v=dUmaWUPRcLQ&ab_channel=Technosage
 
-// Array /Bit Manipulation Problem
-
+// Array; Bit Manipulation Problem
 // Easy
+
 // T.C = o()
 // S.C = o()
 
+/**
+Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+You must implement a solution with a linear runtime complexity and use only constant extra space.
+*/
 /** 
 Example 1:
 Input: nums = [2,2,1]
 Output: 1
-
 */
 public class SingleNumber 
 {
