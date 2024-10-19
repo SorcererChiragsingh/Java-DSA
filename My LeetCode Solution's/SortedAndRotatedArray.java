a3c4c961608a95b484a55c819c423ce6344859cc
Chirag singh <chiragsingh8926@gmail.com>
2024-10-19 19:57:16 +0530

    LeetCode Q.1752:-Check If Array Is Sorted And Rotated

--- My LeetCode Solution's/SortedAndRotatedArray.java
+++ My LeetCode Solution's/SortedAndRotatedArray.java
@@ -4,11 +4,17 @@
 // Solution:- https://www.youtube.com/watch?v=t1GLDWqWVQk&ab_channel=ifelsestatement
 
 // Array Problem
-
 // Easy 
 
 // T.C = o()
 // S.C = o()
+
+/**
+Given an array nums, return true if the array was originally sorted in non-decreasing order, then rotated some number of positions (including zero). Otherwise, return false.
+There may be duplicates in the original array.
+Note: An array A rotated by x positions results in an array B of the same length such that A[i] == B[(i+x) % A.length], where % is the modulo operation.
+*/
+
 /** 
 Example 1:
 
